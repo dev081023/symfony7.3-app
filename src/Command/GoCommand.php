1a4678e0ad10903eec35a1026315c0ad69d10106
Yevhen <yevhen081023@gmail.com>
2025-11-08 20:36:36 +0200

    Command

---
+++ src/Command/GoCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'go',
+    description: 'Add a short description for your command',
+)]
+class GoCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+
+        return Command::SUCCESS;
+    }
+}
